39533d7000d8888c86f4a7400ff4a9c2ac8ce337
moses0823 <moses0823@icloud.com>
2026-09-12 19:18:15 +0800

    隨機splash

--- js/menu.js
+++ js/menu.js
@@ -123,6 +123,33 @@ document.addEventListener(
   }
 );
 
+/* Random splash */
+
+const splashTexts = [
+  "我去，炸木機又忘記關了",
+  "/give @a minecraft:command_block",
+  "/give @a deltaforce:heart_of_africa",
+  "/give @a deltaforce:tear_of_ocean",
+  "服主退游三角洲後決定回MC種田",
+  "誰說自由的，看看服規謝謝",
+  "677676767677676767",
+  "six seven!",
+  "中繼器接紅石粉...放一個tnt在這...Boom!",
+  "六七",
+  "陸柒"
+];
+
+function randomSplash() {
+  const splash = document.getElementById("splash");
+
+  if (!splash) return;
+
+  const randomIndex = Math.floor(Math.random() * splashTexts.length);
+  splash.textContent = splashTexts[randomIndex];
+}
+
+randomSplash();
+
 
 /* =========================================================
    COPY IP
